74bab4af88a6c5584b56f469d1971459ac230f69
Rahul Ranjan <r.rahul.ranjan@oracle.com>
2024-03-09 20:42:56 +0530

    update getmapping to fetch by username too

--- src/main/java/com/example/urlShortener/controller/Controller.java
+++ src/main/java/com/example/urlShortener/controller/Controller.java
@@ -5,6 +5,9 @@ import com.example.urlShortener.repoInterface.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @RestController
 public class Controller {
 
@@ -15,9 +18,31 @@ public class Controller {
         return userRepository.findById(id).orElse(null);
     }
 
+    @GetMapping("/username/{userName}")
+    public User getUserInfoByUsername(@PathVariable String userName) {
+        User user1 =  userRepository.findByUserName(userName);
+        if(user1 == null)
+        {
+            User defaultUser = new User();
+            defaultUser.setUserName(userName+" not found");
+            return defaultUser;
+        }
+        else
+            return user1;
+    }
+
     @PostMapping("/user")
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
+    public String createUser(@RequestBody User user) {
+        User findUser = userRepository.findByUserName(user.getUserName());
+        System.out.println(findUser);
+        if(findUser!=null)
+        {
+            return "User is already there";
+        }
+        else {
+            User createdUser = userRepository.save(user);
+            return "user created with id : "+ createdUser.getId();
+        }
     }
 
     @GetMapping("/hello/{name}")
--- src/main/java/com/example/urlShortener/repoInterface/UserRepository.java
+++ src/main/java/com/example/urlShortener/repoInterface/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.urlShortener.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    User findByUserName(String userName);
 }
