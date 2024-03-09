429da86eec71304b22ccb8c0a66053ad91416e84
Rahul Ranjan <r.rahul.ranjan@oracle.com>
2024-03-09 19:47:41 +0530

    added model, interface and controller

--- src/main/java/com/example/urlShortener/controller/Controller.java
+++ src/main/java/com/example/urlShortener/controller/Controller.java
@@ -1,14 +1,23 @@
 package com.example.urlShortener.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.urlShortener.model.User;
+import com.example.urlShortener.repoInterface.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello there";
+
+    @Autowired
+    private UserRepository userRepository;
+    @GetMapping("/user/{id}")
+    public User getUserInfo(@PathVariable String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
     }
 
     @GetMapping("/hello/{name}")
---
+++ src/main/java/com/example/urlShortener/model/User.java
@@ -0,0 +1,36 @@
+package com.example.urlShortener.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
